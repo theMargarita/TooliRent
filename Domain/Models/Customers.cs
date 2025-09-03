3f0cedb213a43b685ffdc8e8a42695353d872ae6
theMargarita <margotvoistinova@hotmail.se>
2025-09-03 16:00:15 +0200

    added some data but not done yet, had to reprioritise the structure and order of the projects

---
+++ Domain/Models/Customers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class Customers
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateOnly? BithDate { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string Address { get; set; }
+        //public DateTime CreatedAt { get; set; } //ai suggested it but not sure if needed now 
+        //public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Domain/Models/Tools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class Tools
+    {
+        public int ToolId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal PricePerDay { get; set; } //create later a tools times days rented to get total price
+        public bool IsAvailable { get; set; } = true;
+        public DateTime PurchaseDate { get; set; }
+
+        //navigation property
+        public ToolCategory Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
