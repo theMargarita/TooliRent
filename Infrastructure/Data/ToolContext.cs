b64a070efd6d316c2bfd77dc4f17bec67b4ffe89
theMargarita <margotvoistinova@hotmail.se>
2025-09-04 15:50:08 +0200

    added basic db props

---
+++ Infrastructure/Data/ToolContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Core.Models;
+using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ToolContext : DbContext
+    {
+        public ToolContext(DbContextOptions<ToolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tools> Tools { get; set; }
+        public DbSet<ToolCategory> ToolCategories { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
