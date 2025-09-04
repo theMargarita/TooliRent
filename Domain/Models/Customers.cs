a4e3974796536d3db268bf5cc86e989b20b41f3e
theMargarita <margotvoistinova@hotmail.se>
2025-09-04 12:40:33 +0200

    added properties for customers, hopefully these are enough

--- Domain/Models/Customers.cs
+++ Domain/Models/Customers.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Infrastructure.Models
+﻿namespace Infrastructure.Models
 {
     public class Customers
     {
@@ -14,7 +8,7 @@ namespace Infrastructure.Models
         public DateOnly? BithDate { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public string Address { get; set; }
+        public string? Address { get; set; }
         //public DateTime CreatedAt { get; set; } //ai suggested it but not sure if needed now 
         //public DateTime UpdatedAt { get; set; }
     }
