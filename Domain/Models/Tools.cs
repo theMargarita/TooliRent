46f0b81868a56ef8c7c359381a615627278fd96d
theMargarita <margotvoistinova@hotmail.se>
2025-09-05 20:26:45 +0200

    added and changed some props, maybe deleted something to

--- Domain/Models/Tools.cs
+++ Domain/Models/Tools.cs
@@ -5,9 +5,10 @@
         public int ToolId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public decimal PricePerDay { get; set; } //create later a tools times days rented to get total price
-        public bool IsAvailable { get; set; } = true;
-        public DateTime PurchaseDate { get; set; }
+        public decimal Price { get; set; } = 0m; 
+        public int QuantityInStock { get; set; } = 0;
+        public bool IsAvailable => QuantityInStock > 0;
+        //public DateTime PurchaseDate { get; set; }
 
         //foreign key
         public int CategoryId { get; set; }
