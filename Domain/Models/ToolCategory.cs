b5f3d58b5161e7e3f82fc850460d78eb9cb0eb9d
theMargarita <margotvoistinova@hotmail.se>
2025-09-05 20:26:28 +0200

    added and changed some props

--- Domain/Models/ToolCategory.cs
+++ Domain/Models/ToolCategory.cs
@@ -2,7 +2,7 @@
 {
     public class ToolCategory
     {
-        public int ToolCategoryId { get; set; }
+        public int CategoryId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
 
