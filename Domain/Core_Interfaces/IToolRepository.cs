4b2b6f06fd149d36b3140487b553ab8ae9a82e1e
theMargarita <margotvoistinova@hotmail.se>
2025-09-10 22:23:07 +0200

    added and removed some unessary code

--- Domain/Core_Interfaces/IToolRepository.cs
+++ Domain/Core_Interfaces/IToolRepository.cs
@@ -4,10 +4,13 @@ namespace Domain.Core.Core_Interfaces
 {
     public interface IToolRepository : IRepository<Tool>
     {
-        Task<IEnumerable<Tool>> GetToolsWithDetailsAsync();
+        //Task<IEnumerable<Tool>> GetToolsWithDetailsAsync();
         Task<IEnumerable<Tool>> GetAvailableToolsAsync();
         Task<IEnumerable<Tool>> SearchToolsAsync(string searchTerm); //search by name, description, category
         Task<int> GetTotalToolsAsync();
         Task<IEnumerable<Tool>> GetToolsByCategoryAsync(string category);
+        Task<IEnumerable<Tool>> GetToolsByPriceAsync(decimal minPrice, decimal maxPrice);
+        Task<bool>ExistsAsync(string name);
+
     }
 }
