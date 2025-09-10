fcd83699aa9ee5da7d2fadbaf23f66bdc8761670
theMargarita <margotvoistinova@hotmail.se>
2025-09-10 22:24:17 +0200

    added a repository, i hope i did it correctly

---
+++ Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Core.Core_Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly ToolContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(ToolContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public virtual async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if(entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public virtual async Task<bool> ExistsAsync(int id)
+        {
+            return await _dbSet.FindAsync(id) != null;
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            return entity;
+        }
+    }
+}
