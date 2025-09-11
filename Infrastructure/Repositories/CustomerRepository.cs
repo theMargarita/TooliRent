df6ec93e57dd1fa9d753ec870baa0c13ed5d8734
theMargarita <margotvoistinova@hotmail.se>
2025-09-11 23:15:29 +0200

    created a repository for customer - migth need to further implement it

---
+++ Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Core.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(ToolContext context) : base(context)
+        {
+        }
+        public async Task DeleteAsunc(int id)
+        {
+           var removedCustomer =  await _dbSet.FirstOrDefaultAsync(c => c.CustomerId == id);
+            if (removedCustomer != null)
+            {
+                _dbSet.Remove(removedCustomer);
+            }
+        }
+
+        public async Task<Customer?> GetCustomerByEmailAsync(string email)
+        {
+            var customerEmail = await _dbSet.FirstOrDefaultAsync(c => c.Email == email);
+            return customerEmail;
+        }
+
+        public async Task<IEnumerable<Customer>> GetCustomersWithOrdersAsync()
+        {
+            return await _dbSet
+                .Include(c => c.Rentals)
+                .OrderBy(c => c.LastName)
+                .ThenBy(c => c.FirstName)
+                .ToListAsync();
+        }
+
+        public async Task<int> GetTotalCustomersAsync()
+        {
+            return await _dbSet.CountAsync();
+        }
+
+        public async Task<IEnumerable<Customer>> SearchCustomersAsync(string searchTerm)
+        {
+            return await _dbSet
+                .Where(c => c.FirstName.Contains(searchTerm) ||
+                            c.LastName.Contains(searchTerm) ||
+                            c.Email.Contains(searchTerm) ||
+                            c.PhoneNumber.Contains(searchTerm))
+                .ToListAsync();
+        }
+    }
+}
