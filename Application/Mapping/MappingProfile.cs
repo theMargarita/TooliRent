26979fd5140fb7ea249b41536cc6cc1bc7ca6fef
theMargarita <margotvoistinova@hotmail.se>
2025-09-11 15:20:16 +0200

    added a small part for mapping just as a reminder to myself

--- Application/Mapping/MappingProfile.cs
+++ Application/Mapping/MappingProfile.cs
@@ -1,15 +1,24 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using AutoMapper;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Infrastructure.Models;
+using Services.DTOs;
 
 
-//namespace Services.Mapping
-//{
-//    public class MappingProfile : Profile
-//    {
-//        public MappingProfile() { }
-//    }
-//}
+namespace Services.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        //domain model
+        //entity to DTO
+        public MappingProfile() 
+        {
+            //CreateMap<Customer, CustomerReadDTO>()
+            //    .ForMember(dto => dto.FullName, option => option.MapFrom(c => $"{c.FirstName} {c.LastName}"));
+        }
+
+    }
+}
