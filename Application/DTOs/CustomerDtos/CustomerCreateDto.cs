028f7733b48b58e9839dc51050b906c4fed20cf2
theMargarita <margotvoistinova@hotmail.se>
2025-09-13 15:46:48 +0200

    name explains it

---
+++ Application/DTOs/CustomerDtos/CustomerCreateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Services.DTOs.CustomerDtos
+{
+    public class CustomerCreateDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+}
