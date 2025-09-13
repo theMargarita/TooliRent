e36631ad8c49ab1cb25b34a3c924a7880f1052e4
theMargarita <margotvoistinova@hotmail.se>
2025-09-13 14:26:30 +0200

    added some logic for category part in tool - if i have fully understood mapping

--- Application/Mapping/MappingProfile.cs
+++ Application/Mapping/MappingProfile.cs
@@ -16,6 +16,14 @@ namespace Services.Mapping
         //entity to DTO
         public MappingProfile() 
         {
+            CreateMap<Tool, ToolReadDto>()
+                .ForMember(dto => dto.CategoryName, option => option.MapFrom(t => t.Category.Name))
+                .ForMember(dto => dto.CategoryId, option => option.MapFrom(t => t.Category.CategoryId))
+                .ForMember(dto => dto.IsAvailable, option => option.MapFrom(a => a.QuantityInStock > 0)); 
+
+            CreateMap<CreateToolDto, Tool>();
+            CreateMap<ToolUpdateDto, Tool>();
+
             //CreateMap<Customer, CustomerReadDTO>()
             //    .ForMember(dto => dto.FullName, option => option.MapFrom(c => $"{c.FirstName} {c.LastName}"));
         }
