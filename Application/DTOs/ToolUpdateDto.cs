c44e1bd8081162407c005eea34ae3979b470e598
theMargarita <margotvoistinova@hotmail.se>
2025-09-13 14:25:05 +0200

    the name explains it

---
+++ Application/DTOs/ToolUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Services.DTOs
+{
+    public class ToolUpdateDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal PricePerDay { get; set; }
+        public int QuantityInStock { get; set; } = 0;
+        public int CategoryId { get; set; }
+    }
+}
