a770b8887e13b2748ae1a72e54b773d91b9130dc
theMargarita <margotvoistinova@hotmail.se>
2025-09-13 14:23:19 +0200

    created a createtooldto - not sure why yet

---
+++ Application/DTOs/CreateToolDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Services.DTOs
+{
+    public class CreateToolDto
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PricePerDay { get; set; }
+        public int QuantityInStock { get; set; } = 0;
+        public int Category { get; set; } 
+    }
+}
