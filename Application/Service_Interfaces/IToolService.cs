59884c20c68c0000e7977f7e788ea98167cabf77
theMargarita <margotvoistinova@hotmail.se>
2025-09-13 14:27:21 +0200

    added some "for now" methods/tasks and will look at it a bit later

---
+++ Application/Service_Interfaces/IToolService.cs
@@ -0,0 +1,16 @@
+﻿using Services.DTOs;
+
+namespace Services.Service_Interfaces
+{
+    public interface IToolService
+    {
+        Task<IEnumerable<ToolDto>> GetAllToolsAsync();
+        Task<ToolDto> GetToolByIdAsync(int id);
+        Task<ToolDto> AddToolAsync(ToolDto toolDto);
+        Task<ToolDto> UpdateToolAsync(ToolDto toolDto);
+        Task<bool> DeleteToolAsync(int id);
+        Task<IEnumerable<ToolDto>> GetAvailableToolsAsync();
+        Task<IEnumerable<ToolDto>> SearchToolsAsync(string searchTerm);
+
+    }
+}
