c373bf4ceb85bcd07fe1464b8183898cdd171634
theMargarita <margotvoistinova@hotmail.se>
2025-09-13 14:24:48 +0200

    created a toolreaddto for only reading part

---
+++ Application/DTOs/ToolReadDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Services.DTOs
+{
+    public class ToolReadDto
+    {
+        public int ToolId { get; set; } //that is the product id
+        public string Name { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal PricePerDay { get; set; } = 0m;
+        public int QuantityInStock { get; set; } = 0;
+        public bool IsAvailable => QuantityInStock > 0;
+    }
+}
