8129d3d54c2415b8303c1b2792c0121f53fbb2ae
theMargarita <margotvoistinova@hotmail.se>
2025-09-14 10:31:17 +0200

    name explains it

---
+++ Application/DTOs/RentalDtos/RentalCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTOs.RentalDtos
+{
+    public class RentalCreateDto
+    {
+        public int CustomerId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<RentalDetailCreateDto> RentalDetails { get; set; } = new List<RentalDetailCreateDto>();
+    }
+}
