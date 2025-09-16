8e489066bbc2efe5e815fb4af98624fe9989736e
theMargarita <margotvoistinova@hotmail.se>
2025-09-16 11:16:12 +0200

    added dtos for category - might need to add more for category

---
+++ Application/DTOs/CategoryDto/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.DTOs.CategoryDto
+{
+    public class CategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+    }
+}
