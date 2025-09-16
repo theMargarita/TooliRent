c8974535d55f0fb1b2533333aa8d8b1ed5a8584c
theMargarita <margotvoistinova@hotmail.se>
2025-09-16 11:17:04 +0200

    added a update rental dto incase of needing to update an order of rental

---
+++ Application/DTOs/RentalDtos/RentalUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Services.DTOs.RentalDtos
+{
+    public interface RentalUpdateDto
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? CustomerId { get; set; }
+        public List<int>? ToolIds { get; set; } // List of Tool IDs to be rented for the customer
+        public decimal? TotalPrice { get; set; }
+    }
+}
