ebb86d984ecb29cbfc53221dd67184803ff68163
theMargarita <margotvoistinova@hotmail.se>
2025-09-16 11:19:56 +0200

    need some repostirory before i can continue

---
+++ Application/Services/CategoryService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Domain.Core.Core_Interfaces;
+using Services.DTOs.CategoryDto;
+using Services.Service_Interfaces;
+
+namespace Services.Services
+{
+    public class CategoryService : IGategoryService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public Task<IEnumerable<CategoryDto>> GetAllCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CategoryReadDto>> GetCategoryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
