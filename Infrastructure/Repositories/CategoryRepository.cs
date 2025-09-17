6d707a55a60e131435b47949a7c9d1b6c1410103
theMargarita <margotvoistinova@hotmail.se>
2025-09-17 14:47:02 +0200

    added needed data

---
+++ Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Core.Core_Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Models;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ToolDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesWithToolsAsync(string toolName)
+        {
+            return await _dbSet
+                .Include(c => c.Tools)
+                .Where(c => c.Tools
+                .Any(t => t.Name == toolName))
+                .ToListAsync();
+        }
+        public async Task<Category?> GetCategoryWithToolsById(int categoryId)
+        {
+            return await _dbSet
+                .Include(c => c.Tools)
+                .FirstOrDefaultAsync(c => c.CategoryId == categoryId);
+        }
+
+    }
+}
