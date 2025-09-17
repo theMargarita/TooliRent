93bcd3687ab2625c0eeafa14dabd488b5e7e9ab4
theMargarita <margotvoistinova@hotmail.se>
2025-09-17 22:43:39 +0200

    validator when creating category

---
+++ Application/Validators/CreateCategoryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Services.DTOs.CategoryDto;
+using System.Data;
+
+namespace Services.Validators
+{
+    public class CreateCategoryValidator : AbstractValidator<CategoryDto>
+    {
+        public CreateCategoryValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Category name is required.")
+                .MaximumLength(50).WithMessage("Category name must not exceed 50 characters.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Category description must not exceed 200 characters.");
+        }
+    }
+}
