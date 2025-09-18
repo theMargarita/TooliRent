dc301741662429dae6b6f1a3454520f1696c7786
theMargarita <margotvoistinova@hotmail.se>
2025-09-18 20:36:03 +0200

    saw i missed to name a id varible correct

--- Application/DTOs/ToolDtos/ToolCreateDto.cs
+++ Application/DTOs/ToolDtos/ToolCreateDto.cs
@@ -13,6 +13,6 @@ namespace Services.DTOs.ToolDtos
         [Column(TypeName = "decimal(18,2)")]
         public decimal PricePerDay { get; set; }
         public int QuantityInStock { get; set; } = 0;
-        public int Category { get; set; } 
+        public int CategoryId { get; set; } 
     }
 }
