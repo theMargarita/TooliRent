e84fd334145c0b0d9df6134df83569f8d58e5cc9
theMargarita <margotvoistinova@hotmail.se>
2025-09-19 12:55:05 +0200

    Refactor method names in ICustomerRepository

--- Domain/Core_Interfaces/ICustomerRepository.cs
+++ Domain/Core_Interfaces/ICustomerRepository.cs
@@ -5,11 +5,11 @@ namespace Domain.Core.Interfaces
 {
     public interface ICustomerRepository : IRepository<Customer>
     {
-        Task<IEnumerable<Customer>> GetCustomersWithOrdersAsync();
+        Task<IEnumerable<Customer>> GetCustomersWithAscendingOrder();
         Task<Customer?> GetCustomerByEmailAsync(string email);
         //Task<Customer?> GetCustomerByPhoneAsync(string phoneNumber);
         Task<IEnumerable<Customer>> SearchCustomersAsync(string searchTerm); //search by name, email, phone
         Task<int> GetTotalCustomersAsync();
-        Task DeleteAsunc(int id);
+        Task DeleteAsync(int id);
     }
 }
