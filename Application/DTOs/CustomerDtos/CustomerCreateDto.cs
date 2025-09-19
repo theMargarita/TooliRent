58109247fac01c1b178967d20c980fef70c82f2f
theMargarita <margotvoistinova@hotmail.se>
2025-09-19 18:31:55 +0200

    forgot to add birthdate

--- Application/DTOs/CustomerDtos/CustomerCreateDto.cs
+++ Application/DTOs/CustomerDtos/CustomerCreateDto.cs
@@ -4,6 +4,7 @@
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public DateTime? DateOfBirth { get; set; }
         public string Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
